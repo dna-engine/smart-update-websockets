1156d8a7fea35d17f14295681b8df4fdc4e954a7
Dem Pilafian <dem@centerkey.com>
2017-09-24 16:52:18 -0700

    WebSockets server

---
+++ server.js
@@ -0,0 +1,42 @@
+// dnajs-smart-update-websockets
+
+// Modules
+const express =   require('express');
+const http =      require('http');
+const WebSocket = require('ws');
+
+// Web server
+const port = 8777;
+const app =  express();
+app.use(express.static('web'));
+const server = http.createServer(app);
+
+// WebSockets server
+const wsServer = new WebSocket.Server({ server });
+function wsHandleConnection(ws, request) {
+   function wsSend(message) {
+      console.log('Outgoing:', message);
+      ws.send(JSON.stringify(message));
+      }
+   function wsHandleIncoming(string) {
+      const message = JSON.parse(string);
+      console.log('Incoming:', message);
+      }
+   function wsHandleClose(code) {
+      console.log('Disconnected: close code %d', code);
+      }
+   wsSend({ ok: true, note: 'Connection from client accepted' });
+   console.log('New connection from client: %s', request.connection.remoteAddress);
+   ws.on('message', wsHandleIncoming);
+   ws.on('close',   wsHandleClose);
+   }
+wsServer.on('connection', wsHandleConnection);
+
+// Server startup
+function handleServerReady() {
+   const url = 'http://localhost:' + server.address().port;
+   console.log('In your web browser, go to:');
+   console.log(url);
+   console.log('\n--- Server listening (hit CTRL-C to stop server) ---');
+   }
+server.listen(port, handleServerReady);
