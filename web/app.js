3a5ca8c7bca8dc80eaf0f6aa348a9882f348db01
Dem Pilafian <dem@centerkey.com>
2020-01-08 16:17:01 -0800

    Switch to es6 object function notation

--- web/app.js
+++ web/app.js
@@ -3,35 +3,35 @@
 const app = {
    wsUrl: 'ws://localhost:7777/',
    ws: null,  //instance of WebSocket
-   wsSend: (message) => {
+   wsSend(message) {
       message = { timestamp: Date.now(), ...message };
       app.log({ outgoing: message });
       app.ws.send(JSON.stringify(message));
       },
-   wsHandleMessageEvent: (event) => {
+   wsHandleMessageEvent(event) {
       app.log({ incoming: JSON.parse(event.data) });
       },
-   wsHandleConnectEvent: (event) => {
+   wsHandleConnectEvent(event) {
       app.log({ connected: event.target.url });
       app.wsSend({ note: 'Client is connected' });
       },
-   wsInit: () => {
+   wsInit() {
       app.ws = new WebSocket(app.wsUrl);
       app.ws.onopen =   app.wsHandleConnectEvent;
       app.ws.onmessage = app.wsHandleMessageEvent;
       },
-   actionSendMessage: (inputElem) => {
+   actionSendMessage(inputElem) {
       app.wsSend({ text: inputElem.val() });
       },
-   actionDisconnect: () => {
+   actionDisconnect() {
       app.ws.close();
       app.log('*** End ***');
       },
-   log: (value) => {
+   log(value) {
       dna.clone('log', { value: JSON.stringify(value) }, { fade: true, top: true });
       },
-   setup: () => {
+   setup() {
       app.log('*** Start ***');
       app.wsInit();
-      }
+      },
    };
