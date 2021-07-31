744e8d21c61b43902067acbfff0e3c152a98e7a6
dpilafian <dem@centerkey.com>
2021-07-30 18:39:40 -0700

    Updated to ws v8.0 with new constructor

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 // dnajs-smart-update-websockets
 
 // Imports
-import express from 'express';
-import WebSocket from 'ws';
+import express             from 'express';
+import { WebSocketServer } from 'ws';
 
 // Web server
-const port = process.env.port || 7777;
+const port =   process.env.port || 7777;
 const server = express().use(express.static('web')).listen(port);
 server.on('listening', () => console.log('--- Server listening on port:', server.address().port));
 server.on('close',     () => console.log('--- Sever shutdown'));
 
 // WebSockets server
-const wsServer = new WebSocket.Server({ server });
+const wsServer = new WebSocketServer({ server });
 let nextClientId = 0;
 const wsHandleConnection = (ws, request) => {
    const id = nextClientId++;
@@ -25,13 +25,13 @@ const wsHandleConnection = (ws, request) => {
       };
    const wsHandleClose = (code) => {
       console.log(id + '-> Disconnected: close code', code);
-      console.log('--- Total clients:', wsServer.clients.size);
+      console.log('--- Connected clients:', wsServer.clients.size);
       if (!wsServer.clients.size)
          server.close();
       };
    wsSend({ ok: true, id: id, note: 'Connection from client accepted' });
    console.log(id + '-> New connection from client [', request.connection.remoteAddress, ']');
-   console.log('--- Total clients:', wsServer.clients.size);
+   console.log('--- Connected clients:', wsServer.clients.size);
    ws.on('message', wsHandleIncoming);
    ws.on('close',   wsHandleClose);
    };
