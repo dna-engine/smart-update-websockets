7e766233f1a6ad8d3e8c079ff242db08859ecc4b
dpilafian <dem@centerkey.com>
2025-07-12 22:35:39 -0700

    Function .info() is more semantically correct

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ import express from 'express';
 // Web server
 const port =   process.env.port || 7777;
 const server = express().use(express.static('web')).listen(port);
-server.on('listening', () => console.log('--- Server listening on port:', server.address().port));
-server.on('close',     () => console.log('--- Sever shutdown'));
+server.on('listening', () => console.info('--- Server listening on port:', server.address().port));
+server.on('close',     () => console.info('--- Sever shutdown'));
 
 // WebSockets server
 const wsServer = new WebSocketServer({ server });
@@ -16,24 +16,24 @@ let nextClientId = 0;
 const wsHandleConnection = (ws, request) => {
    const id = nextClientId++;
    const wsSend = (message) => {
-      console.log(id + '-> Outgoing:', message);
+      console.info(id + '-> Outgoing:', message);
       ws.send(JSON.stringify(message));
       };
    const wsHandleIncoming = (string) => {
       const message = JSON.parse(string);
-      console.log(id + '-> Incoming:', message);
+      console.info(id + '-> Incoming:', message);
       };
    const wsHandleClose = (code) => {
-      console.log(id + '-> Disconnected: close code', code);
-      console.log('--- Connected clients:', wsServer.clients.size);
+      console.info(id + '-> Disconnected: close code', code);
+      console.info('--- Connected clients:', wsServer.clients.size);
       if (!wsServer.clients.size)
          server.close();
       };
    wsSend({ ok: true, id: id, note: 'Connection from client accepted' });
-   console.log(id + '-> New connection from client [', request.connection.remoteAddress, ']');
-   console.log('--- Connected clients:', wsServer.clients.size);
+   console.info(id + '-> New connection from client [', request.connection.remoteAddress, ']');
+   console.info('--- Connected clients:', wsServer.clients.size);
    ws.on('message', wsHandleIncoming);
    ws.on('close',   wsHandleClose);
    };
-wsServer.on('listening', () => console.log('--- WebSockets ready'));
+wsServer.on('listening', () => console.info('--- WebSockets ready'));
 wsServer.on('connection', wsHandleConnection);
